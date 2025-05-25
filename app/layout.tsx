21433172ea240586e2a9c38be6bb304fc1b45a2b
Bhayga Peramuna <95372450+it21241246@users.noreply.github.com>
2025-05-25 22:28:39 +0530

    add app directory

---
+++ app/layout.tsx
@@ -0,0 +1,55 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "sonner";
+import Header from "@/components/common/header";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import Footer from "@/components/common/footer";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Smart City - AI-Powered Solutions for Smarter Urban Living",
+  description: "AI-Powered Solutions for Smarter Urban Living",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}
+      >
+        <div className="w-full dark:bg-black bg-white dark:bg-dot-white/[0.3] bg-dot-black/[0.3] relative">
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="max-w-7xl mx-auto relative z-20 ">
+              <div className="sticky top-0 z-50 bg-white dark:bg-black">
+                <Header />
+              </div>
+              {children}
+              <Footer />
+            </div>
+            <Toaster position="bottom-right" />
+          </ThemeProvider>
+        </div>
+      </body>
+    </html>
+  );
+}
